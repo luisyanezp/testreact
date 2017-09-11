7bea71f35c8afe2b76d61704f14b4170ce577a7b
Luis Yanez <luisyanez@MacBook-de-Luis.local>
2017-09-10 22:33:28 -0500

    domSept10Noche

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-/* import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
-*/
 import React, { Component } from 'react';
 import fire from './fire';
+import { Button } from 'react-bootstrap';
+import { FormGroup } from 'react-bootstrap';
+import { ControlLabel } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import { HelpBlock } from 'react-bootstrap';
+
 
 class App extends Component {
   constructor(props) {
@@ -45,15 +29,33 @@ class App extends Component {
   }
   render() {
     return (
-      <form onSubmit={this.addMessage.bind(this)}>
-        <input type="text" ref={ el => this.inputEl = el }/>
-        <input type="submit"/>
-        <ul>
-          { /* Render the list of messages */
-            this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
-          }
-        </ul>
-      </form>
+      <div className="App">
+        <div className="App-header">
+          <h2 className="Title">Find My Puesto 2</h2>
+        </div>
+        <span></span>
+        <span></span>
+          <form onSubmit={this.addMessage.bind(this)}>
+            <input type="text" ref={ el => this.inputEl = el }/>
+            <input type="submit"/>
+            <ul>
+              { /* Render the list of messages */
+                this.state.messages.map( message => <li key={message.id}>Mensaje: {message.text}</li> )
+              }
+            </ul>
+            <span></span>
+              <FormGroup>              >
+                <ControlLabel>Working example with validation</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="Enter text"
+                />
+                <FormControl.Feedback />
+                <HelpBlock>Validation is based on string length.</HelpBlock>
+              </FormGroup>
+          </form>
+      </div>
     );
   }
 }
